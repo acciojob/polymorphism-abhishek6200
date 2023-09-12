c6c71e9f27736b23bcb072fc206ea2bac33ababb
Your Name <you@example.com>
2023-09-12 08:35:09 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int x = p.product(4,5);
+        int y = p.product(4,5,6);
+        double z = p.product(4.5, 6.7);
+        System.out.print(x+" "+y+" "+z);
+    }
+
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
 }
